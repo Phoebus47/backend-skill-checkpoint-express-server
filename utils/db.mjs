594c0975a0c350fdbc13472760314b30fcf988d7
Thanakrit <thanakrit.than.biz@gmail.com>
2025-06-18 17:41:22 +0700

    fix: connect to database

--- utils/db.mjs
+++ utils/db.mjs
@@ -4,7 +4,7 @@ const { Pool } = pg.default;
 
 const connectionPool = new Pool({
   connectionString: process.env.DATABASE_URL || 
-    "postgresql://postgres:Tnk-852813@localhost:5432/quora_mock",
+    "postgresql://neondb_owner:npg_P1QznL3gysij@ep-rapid-waterfall-a1bwqcj7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
